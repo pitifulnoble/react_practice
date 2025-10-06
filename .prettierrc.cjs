63b8ee128d83ef9778a806708bde0e6a235e9a56
amos <pitifulnoble@outlook.com>
2025-10-06 17:11:40 +0800

    done

---
+++ .prettierrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  // 120 characters per line
+  printWidth: 120,
+  // use spaces instead of tabs
+  useTabs: false,
+  // 2 spaces per tab
+  tabWidth: 2,
+  // use semicolons
+  semi: true,
+  // use single quotes
+  singleQuote: true,
+  // use single quotes for jsx
+  jsxSingleQuote: true,
+  // use arrow function parens avoid
+  arrowParens: "avoid",
+  // space before and after brackets
+  bracketSpacing: true,
+  // no trailing commas
+  trailingComma: "none",
+};
