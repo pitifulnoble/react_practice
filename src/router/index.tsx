8240b36e264b6e987897212e8b3ad2187f62c803
amos <pitifulnoble@outlook.com>
2025-10-06 19:25:55 +0800

    done

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,19 @@
-import Login from "@/view/Login/Login";
-import { createBrowserRouter } from "react-router-dom";
+import Login from "../views/Login/Login";
+import Error404 from "../views/404";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 
 const router = [
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "*",
+    element: <Navigate to="/404" />,
+  },
+  {
+    path: "/404",
+    element: <Error404 />,
   },
 ];
 
