9db221501eec38985148336f5bf522276bbef198
amos <pitifulnoble@outlook.com>
2025-10-08 00:53:23 +0800

    done

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -13,11 +13,19 @@ export default defineConfig([
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      reactRefresh.configs.vite
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
-  },
-])
+    rules: {
+      'no-console': 'off', // 禁止使用console
+      'no-unused-vars': 'off', // 禁止定义未使用的变量
+      'no-debugger': 'error', // 禁止使用debugger
+      'no-var': 'error', // 要求使用 let 或 const 而不是 var
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/no-unused-vars': 'off'
+    }
+  }
+]);
---
+++ src/types/api.ts
@@ -0,0 +1,179 @@
+// 接口类型定义
+
+export interface Result<T = any> {
+  code: number;
+  data: T;
+  msg: string;
+}
+export interface ResultData<T = any> {
+  list: T[];
+  page: {
+    pageNum: number;
+    pageSize: number;
+    total: number | 0;
+  };
+}
+
+export interface PageParams {
+  pageNum: number;
+  pageSize?: number;
+}
+export namespace Login {
+  export interface params {
+    userName: string;
+    userPwd: string;
+  }
+}
+// 用户管理
+export namespace User {
+  export interface Params extends PageParams {
+    userId?: number;
+    userName?: string;
+    state?: number;
+  }
+
+  export interface SearchParams {
+    userId?: number;
+    userName?: string;
+    state?: number;
+  }
+  export interface UserItem {
+    _id: string;
+    userId: number;
+    userName: string;
+    userEmail: string;
+    deptId: string;
+    state: number;
+    mobile: string;
+    job: string;
+    role: number;
+    roleList: string;
+    createId: number;
+    deptName: string;
+    userImg: string;
+  }
+  export interface CreateParams {
+    userName: string;
+    userEmail: string;
+    mobile?: number;
+    deptId: string;
+    job?: string;
+    state?: number;
+    roleList: string[];
+    userImg: string;
+  }
+  export interface EditParams extends CreateParams {
+    userId: number;
+  }
+}
+// 部门管理
+export namespace Dept {
+  export interface Params {
+    deptName?: string;
+  }
+  export interface CreateParams {
+    deptName: string;
+    parentId?: string;
+    userName: string;
+  }
+  export interface EditParams extends CreateParams {
+    _id: string;
+  }
+  export interface DelParams {
+    _id: string;
+  }
+  export interface DeptItem {
+    _id: string;
+    createTime: string;
+    updateTime: string;
+    deptName: string;
+    parentId: string;
+    userName: string;
+    children: DeptItem[];
+  }
+}
+
+export namespace Menu {
+  export interface Params {
+    menuName: string;
+    menuState: number;
+  }
+  // 菜单创建
+  export interface CreateParams {
+    menuName: string; // 菜单名称
+    icon?: string; // 菜单图标
+    menuType: number; // 1: 菜单 2：按钮 3：页面
+    menuState: number; // 1：正常 2：停用
+    menuCode?: string; // 按钮权限标识
+    parentId?: string; // 父级菜单ID
+    path?: string; // 菜单路径
+    component?: string; // 组件名称
+  }
+  export interface MenuItem extends CreateParams {
+    _id: string;
+    createTime: string;
+    buttons?: MenuItem[];
+    children?: MenuItem[];
+  }
+  export interface EditParams extends CreateParams {
+    _id?: string;
+  }
+
+  export interface DelParams {
+    _id: string;
+  }
+}
+
+export namespace Dashboard {
+  export interface ReportData {
+    driverCount: number;
+    totalMoney: number;
+    orderCount: number;
+    cityNum: number;
+  }
+  export interface LineData {
+    label: string[];
+    order: number[];
+    money: number[];
+  }
+  export interface PieData {
+    value: number;
+    name: string;
+  }
+  export interface RadarData {
+    indicator: Array<{ name: string; max: number }>;
+    data: {
+      name: string;
+      value: number[];
+    };
+  }
+}
+
+export namespace Role {
+  export interface Params extends PageParams {
+    roleName?: string;
+  }
+  export interface CreateParams {
+    roleName: string;
+    remark?: string;
+  }
+  export interface RoleItem extends CreateParams {
+    _id: string;
+    permissionList: {
+      checkedKeys: string[];
+      halfCheckedKeys: string[];
+    };
+    updateTime: string;
+    createTime: string;
+  }
+  export interface EditParams extends CreateParams {
+    _id: string;
+  }
+  export interface Permission {
+    _id: string;
+    permissionList: {
+      checkedKeys: string[];
+      halfCheckedKeys: string[];
+    };
+  }
+}
