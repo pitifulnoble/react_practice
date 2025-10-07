1d79d57191a77af52e7cb68cf110befdb8dea1b5
amos <pitifulnoble@outlook.com>
2025-10-07 19:19:15 +0800

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import { ConfigProvider, App as AntdApp, theme } from 'antd';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#ed6c00'
+        }
+      }}
+    >
+      <RouterProvider router={router} />
+    </ConfigProvider>
+  );
 }
 
 export default App;
