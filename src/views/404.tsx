025d2493f0c918abb08f3eaec2e19d12ec34f800
amos <pitifulnoble@outlook.com>
2025-10-07 18:48:09 +0800

    done

--- src/views/404.tsx
+++ src/views/404.tsx
@@ -1,17 +1,17 @@
-import { Button, Result } from "antd";
-import { useNavigate } from "react-router-dom";
+import { Button, Result } from 'antd';
+import { useNavigate } from 'react-router-dom';
 function NotFound() {
   const navigate = useNavigate();
   const handleClick = () => {
-    navigate("/");
+    navigate('/');
   };
   return (
     <Result
       status={404}
-      title="404"
-      subTitle="抱歉，您访问的页面不存在。"
+      title='404'
+      subTitle='抱歉，您访问的页面不存在。'
       extra={
-        <Button type="primary" onClick={handleClick}>
+        <Button type='primary' onClick={handleClick}>
           回首页
         </Button>
       }
--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,7 +1,29 @@
+import { Button, Form, Input } from 'antd';
+import styles from './index.module.less';
 export default function Login() {
+  const onFinish = () => {
+    console.log('ok');
+  };
   return (
-    <div className="login">
-      <div className="login-wrapper"></div>
+    <div className={styles.login}>
+      <div className={styles.loginWrapper}>
+        <div className={styles.title}>系统登录</div>
+        <Form name='basic' initialValues={{ remember: true }} onFinish={onFinish} autoComplete='off'>
+          <Form.Item name='username' rules={[{ required: true, message: 'Please input your username!' }]}>
+            <Input />
+          </Form.Item>
+
+          <Form.Item name='password' rules={[{ required: true, message: 'Please input your password!' }]}>
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item label={null}>
+            <Button type='primary' block htmlType='submit'>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 }
