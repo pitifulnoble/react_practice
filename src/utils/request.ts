74bdbda0c565671910a8c0c98862bae01fd77303
amos <pitifulnoble@outlook.com>
2025-10-08 00:57:19 +0800

    done

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,8 +1,7 @@
 import axios, { AxiosError } from 'axios';
-import { showLoading, hideLoading } from './loading';
 import storage from './storage';
 import env from '@/config';
-import { Result } from '@/types/api';
+import type { Result } from '@/types/api';
 import { message } from './AntdGlobal';
 console.log('config', env);
 // 创建实例
@@ -18,7 +17,6 @@ const instance = axios.create({
 // 请求拦截器
 instance.interceptors.request.use(
   config => {
-    if (config.showLoading) showLoading();
     const token = storage.get('token');
     if (token) {
       config.headers.Authorization = 'Bearer ' + token;
@@ -41,58 +39,18 @@ instance.interceptors.request.use(
 instance.interceptors.response.use(
   response => {
     const data: Result = response.data;
-    hideLoading();
     if (response.config.responseType === 'blob') return response;
     if (data.code === 500001) {
       message.error(data.msg);
       storage.remove('token');
       location.href = '/login?callback=' + encodeURIComponent(location.href);
     } else if (data.code != 0) {
-      if (response.config.showError === false) {
-        return Promise.resolve(data);
-      } else {
-        message.error(data.msg);
-        return Promise.reject(data);
-      }
+      return Promise.reject(data);
     }
     return data.data;
   },
   error => {
-    hideLoading();
     message.error(error.message);
     return Promise.reject(error.message);
   }
 );
-
-interface IConfig {
-  showLoading?: boolean;
-  showError?: boolean;
-}
-export default {
-  get<T>(url: string, params?: object, options: IConfig = { showLoading: true, showError: true }): Promise<T> {
-    return instance.get(url, { params, ...options });
-  },
-  post<T>(url: string, params?: object, options: IConfig = { showLoading: true, showError: true }): Promise<T> {
-    return instance.post(url, params, options);
-  },
-  downloadFile(url: string, data: any, fileName = 'fileName.xlsx') {
-    instance({
-      url,
-      data,
-      method: 'post',
-      responseType: 'blob'
-    }).then(response => {
-      const blob = new Blob([response.data], {
-        type: response.data.type
-      });
-      const name = (response.headers['file-name'] as string) || fileName;
-      const link = document.createElement('a');
-      link.download = decodeURIComponent(name);
-      link.href = URL.createObjectURL(blob);
-      document.body.append(link);
-      link.click();
-      document.body.removeChild(link);
-      window.URL.revokeObjectURL(link.href);
-    });
-  }
-};
